07edcb34c2983356d1448eda2f14c018c2a28b25
divyagovindaiah <110388603+divyagovindaiah@users.noreply.github.com>
2022-11-03 12:30:29 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh  
 #!/bin/bash
-########################################################## Shell Script to Build Docker Image by FOSSTechNix.com &nbsp; ########################################################DATE=`date +%Y.%m.%d.%H.%M'
+########################################################## Shell Script to Build Docker Image  ########################################################DATE=`date +%Y.%m.%d.%H.%M'
 echo "build the docker image"
 sudo docker build  -t deployimage:1.0.1 .
 echo "built docekr images and list the images"
