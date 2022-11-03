972e4e17188781000b1e407c7601944273943c84
divyagovindaiah <110388603+divyagovindaiah@users.noreply.github.com>
2022-11-03 12:00:08 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 const express= require('express')
 const app= express()
+const fs= require('fs');
+const { resourceLimits } = require('worker_threads');
 
-const port = 3000;
+const port = 8080;
 
 app.listen(port, () => {
-  console.log('Example app listening at http://localhost:3000')
+  console.log(`Example app listening at http://localhost${port}`)
 });
 app.get('/foo', function(req, res) {
-    res.send('Hello World')
+    // res.send('Hello World')
+   const data =  fs.readFileSync('telemetry.json', 'utf-8')
+   res.send(data)
+    
 });
 
 app.use(express.urlencoded({
   extended: true
 }));
  
+
 
